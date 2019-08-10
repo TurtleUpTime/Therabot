4b86c574c48e928b00629ad786902325cfc381cf
TurtleUpTime <53844392+TurtleUpTime@users.noreply.github.com>
2019-08-09 20:22:29 -0400

    Update and rename README.md to index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+# Therabot
+onst Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login('NjA4NDc0MjQ1MDUxMzE4MzM3.XU4NcQ.UJMLNFK2t4aYi8N5xkT79GalECQ');
